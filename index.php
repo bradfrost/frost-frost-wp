0b239a2b49abe2f6888e55916aabe3572fced962
Ian Frost <inf5000@gmail.com>
2017-02-15 18:54:59 -0500

    Refactored the homepage. Added advanced custom fields to insert songs onto page

--- index.php
+++ index.php
@@ -4,73 +4,35 @@
  * @subpackage HTML5-Reset-WordPress-Theme
  * @since HTML5 Reset 2.0
  */
+ 
  get_header(); ?>
 
  <div class="lc-single">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            <ul class="c-crazy-grid">
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-                <li class="c-crazy-grid__item" data-number="3" "title">
-                    <a href="" class="c-crazy-grid__link">Song Link</a>
-                </li>
-            </ul>
+            <?php
+
+            // check if the repeater field has rows of data
+            if( have_rows('songs') ): ?>
+
+                <ul class="c-crazy-grid">
+
+             	<?php // loop through the rows of data
+                while ( have_rows('songs') ) : the_row(); ?>
+
+                    <li class="c-crazy-grid__item" title="<?php the_sub_field('song_title'); ?>" data-number="<?php the_sub_field('number_people'); ?>" data-genre="<?php the_sub_field('song_genre'); ?>">
+                        <a href="#" class="c-crazy-grid__link"><?php the_sub_field('song_title'); ?></a>
+                    </li>
+
+                <?php endwhile;
+
+            else :
+
+                // no rows found
+
+            endif;
+
+            ?>
 
 	<?php endwhile; ?>
 
